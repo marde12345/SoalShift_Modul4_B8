898c3cf9b34ab7d4d5a05df9815e3df338d9dcce
Marde Fasma <mardefasma123up@gmail.com>
2017-11-15 15:20:10 +0700

    [+]readdir + Debugging

--- soal1.c
+++ soal1.c
@@ -21,12 +21,17 @@ static int lala_getattr(const char *gattrpath, struct stat *gastbuff){
 
 static int lala_read(const char *rpath, char *rbuff, size_t rsize, off_t roffset, struct fuse_file_info *rffi ){
 	char rfpath[1001];
-	sprintf(rfpath,"%s%s",dirpath,rpath);
+
+	if(strcmp(rpath,"/")!=0)sprintf(rfpath, "%s%s",dirpath,rpath);
+	else{
+		rpath=dirpath;
+		sprintf(rfpath,"%s",rpath);
+	}
 
 	int rres=0,rfd=0;
 	(void) rffi;
 
-	rfd=open(rfpath,O_RDONLY);
+	rfd=open(rpath,O_RDONLY);
 	if(!(rfd+1)) return -errno;
 
 	rres=pread(rfd,rbuff,rsize,roffset);
@@ -34,6 +39,38 @@ static int lala_read(const char *rpath, char *rbuff, size_t rsize, off_t roffset
 
 	close(rfd);
 	return rres;
+}
+
+static int lala_readdir(const char *rdpath, void *rdbuff, fuse_fill_dir_t rdfiller, off_t rdoffset, struct fuse_file_info *rdffi){
+	char rdfpath[1000];
+	
+	if(strcmp(rdpath,"/")!=0)sprintf(rdfpath, "%s%s",dirpath,rdpath);
+	else{
+		rdpath=dirpath;
+		sprintf(rdfpath,"%s",rdpath);
+	}
+	
+	int rdres = 0;
+	DIR *rddp;
+	struct dirent *rdde;
+
+	(void) rdoffset;
+	(void) rdffi;
+
+	rddp = opendir(rdpath);
+	if (!rddp) return -errno;
+
+	while ((rdde = readdir(rddp))) {
+		struct stat rdst;
+		memset(&rdst, 0, sizeof(rdst));
+		rdst.st_ino = rdde->d_ino;
+		rdst.st_mode = rdde->d_type << 12;
+		rdres = (rdfiller(rdbuff, rdde->d_name, &rdst, 0));
+			if(rdres) break;
+	}
+
+	closedir(rddp);
+	return 0;
 }
 
 static struct fuse_operations st_fuse_op = {
